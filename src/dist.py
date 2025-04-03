b4f4c66cb62524adbd6698e3eee64600c46fb116
shangzhihao <shangzhihao@gmail.com>
2025-04-03 16:21:55 +0800

    exponential, erlang, hyper-erlang

---
+++ src/dist.py
@@ -0,0 +1,179 @@
+from abc import ABC, abstractmethod
+from cmath import phase
+from functools import reduce
+import math
+import re
+import numpy as np
+from numpy.typing import NDArray
+
+
+class AbcPhDist(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+        self.mean = self.getMoment(1)
+        self.variance = self.getMoment(2) - self.mean**2
+        self._moments = {}
+
+    def getMoment(self, k: int) -> float:
+        if k in self._moments:
+            return self._moments[k]
+        else:
+            res = self._calcMoment(k)
+            self._moments[k] = res
+            return res
+
+    @abstractmethod
+    def _calcMoment(self, k: int) -> float:
+        pass
+
+    @abstractmethod
+    def pdf(self, x: float) -> float:
+        pass
+
+    @abstractmethod
+    def cdf(self, x: float) -> float:
+        pass
+
+
+class Exponential(AbcPhDist):
+    def __init__(self, rate: float) -> None:
+        super().__init__()
+        self.rate = rate
+
+    def _calcMoment(self, k: int) -> float:
+        assert k >= 0
+        return math.factorial(k) / self.rate**k
+
+    def pdf(self, x: float) -> float:
+        assert x >= 0
+        return self.rate * math.exp(-self.rate * x)
+
+    def cdf(self, x: float) -> float:
+        assert x >= 0
+        return 1 - math.exp(-self.rate * x)
+
+
+# erlang distribution with phase parameter and rate parameter
+# peak = (phase-1)*rate
+# rate = mean/var
+# phase = mean*rate
+class Erlang(AbcPhDist):
+    def __init__(self, rate: float, phase: int):
+        self.rate = rate
+        self.phase = phase
+
+    def _calcMoment(self, k: int) -> float:
+        res = reduce(lambda x, y: x * y, range(self.phase, self.phase + k))
+        res /= self.rate**self.phase
+        return res
+
+    def pdf(self, x: float) -> float:
+        x1 = self.rate**self.phase
+        x2 = x ** (self.phase - 1)
+        x3 = math.exp(-self.rate * x)
+        return x1 * x2 * x3 / math.factorial(self.phase - 1)
+
+    def cdf(self, x: float) -> float:
+        res = 0
+        for i in range(self.phase):
+            x1 = (self.rate * x) ** i
+            x2 = math.exp(-self.rate * x)
+            res += x1 * x2 / math.factorial(i)
+        return 1 - res
+
+    def get_trans_matrix(self) -> NDArray:
+        res = np.zeros((self.phase, self.phase))
+        for i in range(self.phase):
+            res[i, i] = -self.rate
+            res[i, i + 1] = self.rate
+        return res
+
+
+class HyperErlangBranch:
+    def __init__(self, dist: Erlang, prob: float):
+        self.erlang = dist
+        self.prob = prob
+
+
+# Hyper-Erlang distribution
+# see https://en.wikipedia.org/wiki/Hyper-Erlang_distribution
+
+
+class HyperErlang(AbcPhDist):
+    def __init__(self, branches: list[HyperErlangBranch]):
+        self.branches = branches
+        self.phase = self._get_phase()
+
+    def _get_phase(self) -> int:
+        return sum(branch.erlang.phase for branch in self.branches)
+
+    def get_alpha(self) -> NDArray:
+        res = np.zeros(self.phase)
+        pos = 0
+        for branch in self.branches:
+            res[pos] = branch.prob
+            pos += branch.erlang.phase
+        return res
+
+    def get_trans_matrix(self) -> NDArray:
+        res = np.zeros((self.phase, self.phase))
+        pos = 0
+        for branch in self.branches:
+            k = branch.erlang.phase
+            res[pos : pos + k, pos : pos + k] = branch.erlang.get_trans_matrix()
+            pos += k
+        return res
+
+    def _calcMoment(self, k: int) -> float:
+        trans = self.get_trans_matrix()
+        dim = trans.shape[0]
+        d0inv = np.linalg.inv(-trans)
+        res = self.get_alpha()
+        res *= d0inv ** k
+        res *= np.ones((dim, 1))
+        res = math.factorial(k)
+        return res
+
+    def pdf(self, x: float) -> float:
+        res = 0.0
+        for branch in self.branches:
+            res += branch.erlang.pdf(x) * branch.prob
+        return res
+
+    def cdf(self, x: float) -> float:
+        res = 0.0
+        for branch in self.branches:
+            res += branch.erlang.cdf(x)
+        return 1 - res
+
+    def cdfBranch(self, branch: HyperErlangBranch, x: float) -> float:
+        temp, res = 0.0, 0.0
+        for i in range(branch.erlang.phase):
+            temp = branch.erlang.rate * x
+            temp = temp**i
+            temp = temp * math.exp(-branch.erlang.rate * x)
+            temp = temp / math.factorial(i)
+            res = res + temp
+        return res * branch.prob
+
+
+class PhDist(AbcPhDist):
+    def _calcMoment(self, k: int) -> float:
+        return 0
+
+    def pdf(self, x: float) -> float:
+        return 0
+
+    def cdf(self, x: float) -> float:
+        return 0
+
+
+class MAP(AbcPhDist):
+    def _calcMoment(self, k: int) -> float:
+        return 0
+
+    def pdf(self, x: float) -> float:
+        return 0
+
+    def cdf(self, x: float) -> float:
+        return 0
